7784bdde0f1345697d28c1a001ae080be2a66ba2
peoray <peoray@yahoo.com>
2020-11-03 13:21:56 +0100

    chore: initialize project with vue-sfc-rollup and restructure files/folders

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueInitialsPlaceholder from '@/vue-initials-placeholder.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueInitialsPlaceholder
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-initials-placeholder />
+  </div>
+</template>
---
+++ src/vue-initials-placeholder.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Hello World</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueInitialsPlaceholder', // vue component name
+};
+</script>
+
+<style scoped></style>
